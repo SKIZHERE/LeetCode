676fab2610b74eddfd35f34b32331a2d8c094cc9
SKIZHERE <kumarsaksham67@gmail.com>
2025-06-18 15:15:07 +0530

    solved

---
+++ Day_1.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for n1,i in enumerate(nums):
+            for n2,j in enumerate(nums):
+                if n1 != n2 and i+j == target:
+                    return n1,n2
