ed491e5c6c7346ff195aac054450698803deb0ce
SKIZHERE <kumarsaksham67@gmail.com>
2025-06-25 22:55:47 +0530

    Q27

---
+++ Day_8.py
@@ -0,0 +1,10 @@
+# 27. Remove Element
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+
