2d1a6f2edb7dc65a401a398539e0fc4d1a473278
SKIZHERE <kumarsaksham67@gmail.com>
2025-06-26 21:37:53 +0530

    solved Q27

--- Day_8.py
+++ Day_8.py
@@ -7,4 +7,13 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        n = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                nums[i] = None
+                pass
+            else:
+                nums[n] = nums[i]
+                n+=1
+        return len(nums) - n
 
