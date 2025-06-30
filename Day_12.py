75e1a3ffc890af3b1520df7639c341e7a474f878
SKIZHERE <kumarsaksham67@gmail.com>
2025-06-30 16:08:29 +0530

    Solved Q66

--- Day_12.py
+++ Day_12.py
@@ -6,3 +6,9 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
+        x = 0
+        for i,obj in enumerate(reversed(digits)):
+            x = x+obj*(10**i)
+        x = str(x+1)
+        newDigits = [int(y) for y in x]
+        return newDigits
