d43ddfc07eadcd8059342519df6fa47a3e786cb7
SKIZHERE <kumarsaksham67@gmail.com>
2025-07-05 23:14:09 +0530

    Solved Q119

---
+++ Day_19.py
@@ -0,0 +1,15 @@
+class Solution(object):
+
+    def getRow(self, rowIndex):
+        numRows = rowIndex + 1
+        x = [[1]]
+        if numRows == 1:
+            return x[rowIndex]
+        x.append([1,1])
+        for i in range (1,numRows-1):
+            z = [1, 1]
+            for j in range(len(x[i])-1):
+                n = x[i][j] + x[i][j+1]
+                z.insert(j+1, n)
+            x.append(z)
+        return x[rowIndex]
