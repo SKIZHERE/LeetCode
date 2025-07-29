da4ffc318abb0993dc52043de869726d2d05cf4d
SKIZHERE <kumarsaksham67@gmail.com>
2025-07-29 09:15:23 +0530

    Solved Q231

---
+++ Day_41.py
@@ -0,0 +1,10 @@
+# 231. Power of Two
+
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and (n & (n - 1)) == 0 # & is a bitwise operator very useful to know
