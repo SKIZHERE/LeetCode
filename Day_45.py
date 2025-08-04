ef0840da459a12608a8e40165f97930cd62bbb4c
SKIZHERE <kumarsaksham67@gmail.com>
2025-08-04 15:11:33 +0530

    Solved Q258

---
+++ Day_45.py
@@ -0,0 +1,11 @@
+# 258. Add Digits
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num > 9:
+            num = sum([int(i) for i in str(num)])
+        return num
