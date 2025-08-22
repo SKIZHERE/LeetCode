91890183487ca0291318bb85ebc519cb67d5b2ae
SKIZHERE <kumarsaksham67@gmail.com>
2025-08-22 21:59:46 +0530

    Solved Q263

---
+++ Day_46.py
@@ -0,0 +1,23 @@
+# 263. Ugly Number
+
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<=0:
+            return False
+        if n == 1:
+            return True
+        while n>1:
+            if n%2 == 0:
+                n = n//2
+            elif n%3 == 0:
+                n = n//3
+            elif n%5 == 0:
+                n = n//5
+            else:
+                return False
+            if n == 1:
+                return True
