572ed17061429fec026df6b249a5f8b77aa025fd
SKIZHERE <kumarsaksham67@gmail.com>
2025-12-13 21:40:03 +0530

    solved Q342

---
+++ Day_53.py
@@ -0,0 +1,10 @@
+#342. Power of Four
+
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and (n & (n - 1)) == 0 and n % 3 == 1
+
