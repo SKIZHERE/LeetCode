8974b96e88222781ba7446ef81578fe1bef8a0e9
SKIZHERE <kumarsaksham67@gmail.com>
2025-12-22 20:48:28 +0530

    solved Q104

---
+++ Day_60.py
@@ -0,0 +1,19 @@
+# 104. Maximum Depth of Binary Tree
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
